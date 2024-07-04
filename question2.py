969c50ab0a1bc0495a6267942f8085e67b80ae1b
zainafzalch <zainafzal6610b@gmail.com>
2024-07-04 17:13:55 -0400

    question2 added

---
+++ question2.py
@@ -0,0 +1,11 @@
+#Task 1: Code Correction You have been given a piece of code with several variable and constant names that don't follow the Python naming convention. Your task is to correct them:
+
+# Pi_value = 3.14
+# userAge = 25
+# user_location = "New York"
+# MAXLIMIT = 1000
+
+PI = 3.14
+user_age = 25
+user_location = "New York"
+max_limit = 1000
